07a6969a7290c5c213e36737427145869cf1184e
LuvArora443 <139987286+LuvArora443@users.noreply.github.com>
2023-07-22 19:31:41 +0530

    Add files via upload

---
+++ env.js
@@ -0,0 +1,9 @@
+const process={
+    env:{
+        OPEN_API_KEY:"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",
+        OPEN_API_KEY1:"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"
+    }
+}
+
+export default process;
+
